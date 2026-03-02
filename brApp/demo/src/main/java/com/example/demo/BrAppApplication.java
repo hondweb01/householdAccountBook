2763dd742ac5475ff3e57b19b05b3bebb0f7b39d
Your Name <you@example.com>
2026-03-02 21:28:10 +0900

    docker追加加

---
+++ brApp/demo/src/main/java/com/example/demo/BrAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrAppApplication.class, args);
+		System.out.println("Hello, W!");
+	}
+
+}
